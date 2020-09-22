37aa63f3affa41adcca37fa9e84b374be3c61663
Shivanirao2000 <shivanirao2000@gmail.com>
2020-09-23 00:37:17 +0530

    Updates

---
+++ precache-manifest.fe7afe95c9dcbd2b4a4496c49aeb3f7c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c8a93706657c87c9211e9d5aae44459",
+    "url": "/Crud-Application/index.html"
+  },
+  {
+    "revision": "5159a8ba3261b2635ba3",
+    "url": "/Crud-Application/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "ae90f8ecda8ac22ef77e",
+    "url": "/Crud-Application/static/css/main.55622ffb.chunk.css"
+  },
+  {
+    "revision": "5159a8ba3261b2635ba3",
+    "url": "/Crud-Application/static/js/2.47613388.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Crud-Application/static/js/2.47613388.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae90f8ecda8ac22ef77e",
+    "url": "/Crud-Application/static/js/main.27731954.chunk.js"
+  },
+  {
+    "revision": "53ebaed7053200ce48e5",
+    "url": "/Crud-Application/static/js/runtime-main.ed82d144.js"
+  }
+]);
